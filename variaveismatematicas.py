85b60dd9de46e5511b6018d232670749f589b0ec
Cristhian Bini <100438104+cristhianbini@users.noreply.github.com>
2022-02-26 15:27:51 -0300

    Create variaveismatematicas.py

---
+++ variaveismatematicas.py
@@ -0,0 +1,25 @@
+nome = "João"
+peso = 50.3
+idade = 30
+print("Nome: {} Peso: {} Idade: {}".format (nome, peso, idade))
+
+x = 10
+y = 30
+
+print(x)
+print(y)
+
+soma = x + y
+print('Soma: {}'.format(soma))
+
+subtracao = x - y
+print("subtração: {}".format(subtracao))
+
+multiplicacao = x * y
+print("Multiplicação: {}".format(multiplicao))
+
+divisao = x / y
+print("Divisão: {}".format(divisao))
+
+potencia = x**2
+print("Potencia: {}".format(potencia))
