bc480e85c06c3f2be6621de5c2113a713ba1aa4f
Cristhian Bini <100438104+cristhianbini@users.noreply.github.com>
2022-02-27 20:09:58 -0300

    Create desvios.py

---
+++ desvios.py
@@ -0,0 +1,83 @@
+#Maior ou igual a 19
+idade = 19
+
+if idade >=18:
+    print("maior de idade")
+    print("1")
+else:
+    print("menor de idade")
+    print("2")
+
+print("3")
+
+idade = 19
+
+#Maior que 18
+if idade > 18:
+    print("maior de idade")
+    print("1")
+else:
+    print("menor de idade")
+    print("2")
+
+print("3")
+
+idade = 19
+
+#Igual a 18
+if idade >= 18:
+    print("maior de idade")
+    print("1")
+else:
+    print("menor de idade")
+    print("2")
+
+print("3")
+
+#Se ela é menor que 18
+idade = 19
+
+if idade < 18:
+    print("maior de idade")
+    print("1")
+else:
+    print("menor de idade")
+    print("2")
+
+print("3")
+
+idade = 19
+
+#Se ela é igual a 18
+if idade == 18:
+    print("maior de idade")
+    print("1")
+else:
+    print("menor de idade")
+    print("2")
+
+print("3")
+
+#Se os valores são diferentes
+idade = 19
+
+if idade != 18:
+    print("maior de idade")
+    print("1")
+else:
+    print("menor de idade")
+    print("2")
+
+print("3")
+
+#Media de notas para aluno
+
+media = 7
+
+if media >= 7:
+    print("Aprovador!")
+else:
+    if media < 6:
+        print("Reprovado!")
+    else:
+        print("Recuperação")
