b3d9b452758871dcd7dd497e07783731d2c61621
Cristhian Bini <100438104+cristhianbini@users.noreply.github.com>
2022-03-02 13:02:05 -0300

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,20 @@
+for numero in range(0,5):
+    print("Olá Mundo!")
+
+for numero in range(0,5):
+    print(numero)
+
+for numero in range(1,5):
+    if numero > 2:
+        print("Numero é maior que 2!")
+    else:
+        print("Numero não é maior que 2!")
+
+for letra in "cristhian":
+    print(letra)
+
+quantidade = 0
+for letra in "batata":
+    if letra == "a":
+        quantidade = quantidade + 1
+print ("Total de letras A = ", quantidade)
